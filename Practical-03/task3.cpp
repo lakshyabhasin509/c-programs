1ad3c3683e7fd25706e49624b4660a7988886cd5
lakshyabhasin509 <75601972+lakshyabhasin509@users.noreply.github.com>
2021-10-02 21:35:41 +0530

    Add files via upload

---
+++ Practical-03/task3.cpp
@@ -0,0 +1,19 @@
+// Simple C++ program to display "Hello World"
+
+// Header file for input output functions
+#include<iostream>
+
+using namespace std;
+
+// main function -
+// where the execution of program begins
+int main()
+{
+	char c;
+
+	getline(cin,line);
+	cout<<c;
+	
+	return 0;
+}
+
